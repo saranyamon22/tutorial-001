1f5eeca198833bf90e3d5b81ddd8949953dbbc8b
LAPTOP-EKA7766I\User <rebornharu12345@gmail.com>
2022-02-08 23:44:15 +0700

    backend

---
+++ backend/app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express()
+app.use(express.json())
+
+const mongoose = require('mongoose');
+const Product = require('./models/product')
+mongoose.connect('mongodb://localhost:27017/tutorial', { useNewUrlParser: true }).then(() => {
+    console.log("connect success")
+})
+
+//API
+app.post('/addProduct', async (req, res) => {
+    const payload = req.body
+    let newProd = new Product(payload)
+    let save = await newProd.save()
+    res.send(save)
+})
+
+app.get('/findProduct', async (req, res) => {
+    let result = await Product.find({})
+    res.send(result)
+})
+
+app.post('/updateProduct', async (req, res) => {
+    let payload = req.body
+    let id = payload.id
+    let data = payload.data
+    await Product.updateOne({ _id: id }, data)
+    let prod = await Product.find({ _id: id })
+    res.send(prod)
+})
+
+app.get('/deleteProduct', async (req, res) => {
+    let payload = req.query
+    let id = payload.id
+    await Product.deleteOne({ _id: id })
+    res.send('deleted')
+})
+
+
+
+app.listen(9000)
